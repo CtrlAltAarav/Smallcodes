0d8dc9ac9bbcf27991b7add028a38c93b8ab958b
Aarav Mithaiwala <xyzaarav45@gmail.com>
2025-05-30 14:56:00 +0530

    CheckLeapyear.py
    This program checks the year entered by the user if it is a leap year or not

---
+++ CheckLeapyear.py
@@ -0,0 +1,6 @@
+# leap_year
+year = int(input("Enter a year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
